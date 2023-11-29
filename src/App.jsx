7da5579a1f4e8015c86809c7747d7cb1c72a8c57
Marina Tanaka <huttenloucher@gmail.com>
2023-11-29 11:10:41 -0300

    utilizando extensão prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 // Todos componentes react devem começar com letra maiuscula
 
-// Vamos utilizar o componente (Fragment <></>) para retornar nosso html
+// Vamos utilizar o componente (Fragment <></>) e envolver o return entre parenteses para retornar nosso html.
 function App() {
-  return <> 
-          <header>
-            <h1>Olá React! -  @_Tanaka11_</h1>
-          </header>
+  return (
+    <>
+      <header>
+        <h1>Olá React! - @_Tanaka11_</h1>
+        <hr />
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">Produto</a>
+          <a href="#">Contato</a>
+        </nav>
+      </header>
 
-          {/* Comentário dentro do JSX*/}
-          
-          <main>
-            <h2>Salve</h2>
-          </main> 
-        </>
+      {/* Comentário dentro do JSX*/}
+
+      <main>
+        <h2>Salve</h2>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
