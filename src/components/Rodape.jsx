ad44e4491de4af3c33a5603579331511312c54ec
Marina Tanaka <huttenloucher@gmail.com>
2023-11-30 09:42:38 -0300

    Exemplos de css com js

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,16 +1,21 @@
 function Rodape() {
+  const exemploEstilos = {
+    textAlign: "center",
+    backgroundColor: "darkblue",
+    color: "white",
+    padding: "2%",
+    border: "solid 4px cornflowerblue",
+  };
+
   return (
     <>
-      {/* 1º par de chaves representa que é um elemento js e o 2º para a tratativa de CSS inline */}
-      <footer
-        style={{
-          textAlign: "center",
-          backgroundColor: "darkblue",
-          color: "white",
-          padding: "2%",
-        }}
-      >
-        <h2>@_Tanaka11_ 🥶</h2>
+      {/* Versão direta:
+       Utilizamos uma constante com os dados do CSS*/}
+      <footer style={exemploEstilos}>
+        {/* Versão inline:
+       1º par de chaves representa que é um elemento JS e o 2º é do objeto para a tratativa de CSS inline 
+      */}
+        <h2 style={{ textAlign: "center" }}>@_Tanaka11_ 🥶</h2>
       </footer>
     </>
   );
