bfeacd36b33cf3fa2339ae876350605a4949d7b3
Marina Tanaka <huttenloucher@gmail.com>
2023-11-30 09:33:41 -0300

    css inline com js

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,8 +1,18 @@
 function Rodape() {
   return (
-    <footer>
-      <h2>@_Tanaka11_ 🥶</h2>
-    </footer>
+    <>
+      {/* 1º par de chaves representa que é um elemento js e o 2º para a tratativa de CSS inline */}
+      <footer
+        style={{
+          textAlign: "center",
+          backgroundColor: "darkblue",
+          color: "white",
+          padding: "2%",
+        }}
+      >
+        <h2>@_Tanaka11_ 🥶</h2>
+      </footer>
+    </>
   );
 }
 
