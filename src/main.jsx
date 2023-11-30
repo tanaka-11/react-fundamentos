343198ebc1dc5cdd833711f726903950da05bd49
Marina Tanaka <huttenloucher@gmail.com>
2023-11-30 09:58:20 -0300

    criado css externo e importado na main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 // Importando as libs ESSENCIAIS (React e ReactDOM).
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 // Importando o principal componente, arquivo em que carregamos os elementos da página.
-import App from './App.jsx'
+import App from "./App.jsx";
+
+// Importando o CSS externo
+import "./index.css";
 
 // Iniciando os recursos do React, manipulando a div#root existente na index.html
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
