cbab394a411bfead59e877dc7e3a5b322cd956a4
Marina Tanaka <huttenloucher@gmail.com>
2023-12-07 11:29:00 -0300

    adicionado import de pages e adicionado componentes dentro de container

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 // Todos componentes react devem começar com letra maiuscula
 
-// Importação do componente
+// Importação dos componentes
 import Cabecalho from "./components/Cabecalho";
 import Conteudo from "./components/Conteudo";
 import Rodape from "./components/Rodape";
+
+// Importação dos componentes dentro do pages
+import Home from "./pages/Home";
+import Produtos from "./pages/Produtos";
+import Contato from "./pages/Contato";
+import Container from "./components/Container";
 
 // Vamos utilizar o componente (Fragment <></>) e envolver o return entre parenteses para retornar nosso html.
 function App() {
@@ -12,7 +18,11 @@ function App() {
       {/* Importando o componente individual envolvido por uma tag */}
       <Cabecalho />
 
-      <Conteudo />
+      <Container>
+        <Home />
+        <Produtos />
+        <Contato />
+      </Container>
 
       <Rodape />
     </>
