4400a91de95ce8e81be4ac0fbbe508c476c29858
Marina Tanaka <huttenloucher@gmail.com>
2023-12-07 11:58:02 -0300

    Adicionado caminho das rotas

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -31,9 +31,9 @@ const StyledMenu = styled.nav`
 function Menu() {
   return (
     <StyledMenu>
-      <a href="">Home</a>
-      <a href="">Produtos</a>
-      <a href="">Contato</a>
+      <a href="/">Home</a>
+      <a href="/produtos">Produtos</a>
+      <a href="/contato">Contato</a>
     </StyledMenu>
   );
 }
