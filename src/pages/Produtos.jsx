90e9f8bfcb6478af47aab6bb795cf160f7da97e8
Marina Tanaka <mtanakasg@gmail.com>
2024-01-15 11:07:43 -0300

    Adicionado hook state para produtos

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,4 +1,10 @@
+// React Hook
+import { useState } from "react";
+
 function Produtos() {
+  // O state "produtos" inicia como um array vazio e só após o carregamento dos dados da API que ele será preenchido
+  const [produtos, setProdutos] = useState([]);
+
   // Carregando dados da Api via função assincrona
   const carregarDados = async () => {
     try {
