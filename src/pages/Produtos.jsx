796af79d743dc23846ab97270fcc4d466d556347
Marina Tanaka <mtanakasg@gmail.com>
2024-01-15 11:28:37 -0300

    adicionado o hook effect com os dados da api

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,23 +1,32 @@
 // React Hook
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Produtos() {
   // O state "produtos" inicia como um array vazio e só após o carregamento dos dados da API que ele será preenchido
   const [produtos, setProdutos] = useState([]);
 
-  // Carregando dados da Api via função assincrona
-  const carregarDados = async () => {
-    try {
-      const resposta = await fetch(`https://fakestoreapi.com/products`);
-      const dados = await resposta.json();
-      console.log(dados);
-      setProdutos(dados);
-    } catch (error) {
-      console.error("Erro: " + error);
-    }
-  };
+  // Gerenciando o efeito colateral do componente com o useEffect atraves de uma função callback para o carregamento de dados da API
 
-  carregarDados();
+  /* Funcionamento do useEffect
+   1º useEffect é carregado uma vez apos a montagem da page.
+   2º ele executa a função "carregarDados"
+   3º atualização do state
+  */
+  useEffect(() => {
+    // Carregando dados da API via função assincrona
+    const carregarDados = async () => {
+      try {
+        const resposta = await fetch(`https://fakestoreapi.com/products`);
+        const dados = await resposta.json();
+        console.log(dados);
+        setProdutos(dados);
+      } catch (error) {
+        console.error("Erro: " + error);
+      }
+    };
+
+    carregarDados();
+  }, []); // Apos o 1º parametro(função) passar o 2º parametro(lista de dependencias)
 
   return <h1>Produtos</h1>;
 }
