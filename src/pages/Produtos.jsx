3166dbbdaf01242f5939f552d5fdf4b4b242317e
Marina Tanaka <mtanakasg@gmail.com>
2024-01-15 10:52:05 -0300

    Adicionando dados de uma api com função assincrona

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,4 +1,17 @@
 function Produtos() {
+  // Carregando dados da Api via função assincrona
+  const carregarDados = async () => {
+    try {
+      const resposta = await fetch(`https://fakestoreapi.com/products`);
+      const dados = await resposta.json();
+      console.log(dados);
+    } catch (error) {
+      console.error("Erro: " + error);
+    }
+  };
+
+  carregarDados();
+
   return <h1>Produtos</h1>;
 }
 
