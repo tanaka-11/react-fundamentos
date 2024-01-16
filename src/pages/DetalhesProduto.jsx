4ecc590e82d706e625d62fad3c5822c196683538
Marina Tanaka <mtanakasg@gmail.com>
2024-01-16 09:33:18 -0300

    adcionado hook "useEffect" com id dinamico

--- src/pages/DetalhesProduto.jsx
+++ src/pages/DetalhesProduto.jsx
@@ -1,8 +1,24 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function DetalhesProduto() {
   // Usando o hook "useParams" com o parametro que foi passado na rota dinamica.
   const { id } = useParams();
+
+  // Função callback com os dados da API e ID dinamico
+  useEffect(() => {
+    const carregarDados = async () => {
+      try {
+        const resposta = await fetch(`https://fakestoreapi.com/products/${id}`); // Passado o id dinamico
+        const dados = await resposta.json();
+        console.log(dados);
+      } catch (error) {
+        console.error("Erro ao carregar produto" + error);
+      }
+    };
+
+    carregarDados;
+  }, [id]); // Passando o id como dependencia do useEffect
 
   return (
     <article>
